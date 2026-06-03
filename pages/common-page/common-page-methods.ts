ad3ca17306cb9b9660fd62ee963c03b173097e71
edmar-qa <musiccheli@gmail.com>
2026-06-03 15:11:07 -0400

    fix page variable type

--- pages/common-page/common-page-methods.ts
+++ pages/common-page/common-page-methods.ts
@@ -1,8 +1,11 @@
-export class commonPageMethods{
-constructor(page){
+
+import {Page} from 'playwright'
+export class CommonPageMethods{
+    private page: Page
+    constructor(page: Page){
     this.page = page
 }
     async navigateToTheApplication(){
-        await this.page.goto{}
+        await this.page.goto('https://www.saucedemo.com/')
     }
 }
