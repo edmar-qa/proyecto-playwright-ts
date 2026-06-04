31ef550a27f3dbd34676a5b7baaa6b7fbb8e3ad0
edmar-qa <musiccheli@gmail.com>
2026-06-04 16:28:56 -0400

    commit allure

--- pages/common-page/common-page-methods.ts
+++ pages/common-page/common-page-methods.ts
@@ -1,6 +1,6 @@
 import { Page } from 'playwright'
 import { CommonPageElements } from './common_page.elements'
-//import { Logger } from '../../support/logger'
+import { Logger } from '../../support/logger'
 export class CommonPageMethods {
     private page: Page
     private commonPageElements: CommonPageElements
@@ -11,32 +11,32 @@ export class CommonPageMethods {
     }
 
     async navigateToTheApplication() {
-      //  await Logger.logStep('Navigate to the Application')
+        await Logger.logStep('Navigate to the Application')
         await this.page.goto('https://www.saucedemo.com/v1/index.html')
     }
 
     async openMenu() {
-       // await Logger.logStep('Click on Open Menu icon')
+        await Logger.logStep('Click on Open Menu icon')
         await this.commonPageElements.buttons.openMenu.click()
     }
 
     async clickOnAllItemsOption() {
-       // await Logger.logStep('Click on All Items option')
+        await Logger.logStep('Click on All Items option')
         await this.commonPageElements.letfMenu.allItems.click()
     }
 
     async clickOnAboutOption() {
-      //  await Logger.logStep('Click on About option')
+        await Logger.logStep('Click on About option')
         await this.commonPageElements.letfMenu.about.click()
     }
 
     async clickOnLogOutOption() {
-     //   await Logger.logStep('Click on Logout option')
+        await Logger.logStep('Click on Logout option')
         await this.commonPageElements.letfMenu.logout.click()
     }
 
     async clickOnResetAppStateOption() {
-       // await Logger.logStep('Click on Reset App State')
+        await Logger.logStep('Click on Reset App State')
         await this.commonPageElements.letfMenu.resetAppState.click();
     }
 }
