a85540149f6567d92f0601b226aa8318ed464800
edmar-qa <musiccheli@gmail.com>
2026-06-04 16:08:07 -0400

    commit

--- pages/products-page/products-page.methods.ts
+++ pages/products-page/products-page.methods.ts
@@ -1,6 +1,6 @@
 import { Page } from 'playwright'
 import { ProductsPageElements } from './products-page.elements'
-import { Logger } from '../../support/logger'
+//import { Logger } from '../../support/logger'
 import { expect } from '@playwright/test'
 export class ProductsPageMethods {
     private page: Page
@@ -12,17 +12,17 @@ export class ProductsPageMethods {
     }
 
     async clickOnAddToCart(productName: string) {
-        await Logger.logStep(`Click on Add To Cart button corresponding to "${productName}"`)
+       // await Logger.logStep(`Click on Add To Cart button corresponding to "${productName}"`)
         await this.productsPageElements.addCartButton(productName).click()
     }
 
     async clickOnCartIcon() {
-        await Logger.logStep('Click on Cart icon')
+        //await Logger.logStep('Click on Cart icon')
         await this.productsPageElements.icons.cart.click();
     }
 
     async verifyProductsPageIsDisplayed() {
-        await Logger.logVerification('Verify that the Products page is shown')
+       // await Logger.logVerification('Verify that the Products page is shown')
         const elementsCount = await this.productsPageElements.otherElements.pageTitle.count()
         expect(elementsCount).toEqual(1)
     }
